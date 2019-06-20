315faa7406ccf26f92740733ec5b779cdd8a4d69
Karma Tawil <tawilkarma@gmail.com>
2019-06-20 14:33:28 +0200

    Création du CSS pour le header et le logo, inclusion du logo en format SVG

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+          <header className="App-header">
+              <div className="App-logo">
+                  <img src={logo} />
+              </div>
+
+          </header>
     </div>
   );
 }
