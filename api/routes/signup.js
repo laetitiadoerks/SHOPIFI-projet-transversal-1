b777af03c98e2d8405985e47350d1d53fbd107de
laetitia doerks <laetitia.doerks@bluewin.ch>
2019-06-21 18:15:14 +0200

    ajout de la fonction singup dans api

--- api/routes/signup.js
+++ api/routes/signup.js
@@ -6,46 +6,49 @@ var connection     = require('../lib/dbconn');
 //    res.send('API is working properly');
 //});
 
-// check email unique
-const emailUnique = async(emailUser) => {
-    const query ="";
+// requete pour check email unique
+const emailUnique =  async(emailUser) => {
+    const query = "SELECT email FROM `user` WHERE email = ?;"
+    var [results] = await connection.promise().query(query, emailUser);
+    console.log('email unique');
+    console.log(results);
+    return  results;
 }
 //pour s'enregistrer
 router.post('/', async (req, res) => {
     try {
         //let user = req.body
         //console.log(user);
-        const idUser = 'null';
+        const idUser = null;
         console.log(idUser);
         const statutUser = 'visiteur';
         console.log(statutUser);
         //console.log([req.body.prenom_user, req.body.nom_user, req.body.genre, req.body.date_naissance, req.body.addresse, req.body.email, req.body.mot_de_passe]);
 
         if (!req.body.email) {
-            throw 'Email requis'
+            throw 'Email requis.'
         }
         if (!req.body.mot_de_passe) {
-            throw 'Mot de passe requis'
+            throw 'Mot de passe requis.'
         }
-//(?, ?, ?, ?, ?, ?, ?, ?, ?)
+        // requete check si email déjà dans BD
+        const listeEmail = await emailUnique(req.body.email);
+        console.log('liste email');
+        console.log(listeEmail);
+
+        if (listeEmail.length != 0){
+            throw 'Cet email est déjà pris.'
+        }
+
         console.log('hihihi');
         const user = [idUser, req.body.prenom_user, req.body.nom_user, req.body.genre, req.body.date_naissance, req.body.addresse, req.body.email, req.body.mot_de_passe, statutUser];
         console.log(user);
-        const query = "INSERT INTO user (id_user, prenom_user, nom_user, genre, date_naissance, addresse, email, mot_de_passe, statut) VALUES ?"
-        const query2 ="INSERT INTO user SET ?"
-        const query3 = "INSERT INTO `user`(`id_user`, `prenom_user`, `nom_user`, `genre`, `date_naissance`, `addresse`, `email`, `mot_de_passe`, `statut`) VALUES (null, 'albertine','rigi','m','1999-12-10','jjjjjjjj','rigi.com','1234','visiteur')";
-        const [results] = await database.query(query3);
+        const query = "INSERT INTO user VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);"
+        await connection.promise().query(query, user)
+        // console.log('hahaha quand meme donnees entree dans BD');
         //console.log(results);
         res.send({'success': true})
 
-            // var user=req.session.username;
-            // console.log(user);
-            // const query = "SELECT prenom_user, nom_user, nom_produit, prix, date_achat FROM user, achat, produit WHERE user.id_user=achat.id_user and achat.id_produit=produit.id_produit and user.email=?"
-            //
-            // const [results] = await connection.promise().query(query, user)
-            // console.log(results);
-            //
-            // res.send(results)
     } catch (err) {
         // Traiter l'erreur (qui est contenue dans `e`)
         res.status(500).send({'error': err})
