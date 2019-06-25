e87586a0c162f253c164c700eaa3e5b540c11e72
Karma Tawil <tawilkarma@gmail.com>
2019-06-25 04:28:14 +0200

    changement

--- client/client/src/components/NavBar.js
+++ client/client/src/components/NavBar.js
@@ -4,41 +4,28 @@ import logo from '../logo.svg';
 import icone from '../icone.svg';
 import styled from 'styled-components';
 import { ButtonContainer } from './button.js';
+import './NavBar.css';
 
 export default class NavBar extends React.Component {
     render() {
         return (
-            <NavWrapper className="navbar navbar-exapand-sm navbar-dark px-sm-5">
+            <div className="App-header">
                 {/*Ou sera renvoyer l'utilisateur quand il clique sur le logo*/}
                 <Link to='/'>
-                    <img src={icone} alt="logo 2" className="navbar-brand" />
+                    <img src={icone} alt="logo 2"  />
                     {/*<img src={logo} alt="logo" className="navbar-brand" >*/}
-                 </Link>
-                <ul className="navbar-nav align-items-center">
-                    <li className="nav-item ml-5">
-                        <Link to="/" className="nav-link">
-                            Products
-                        </Link>
-                    </li>
-                </ul>
-                <Link to="/cart" className="ml-auto">
-                    <ButtonContainer>
-                        <span className="mr-2"> 
-                        <i className="fas fa-cart-plus" />
-                        my cart
-                        </span>
-                    </ButtonContainer>
                 </Link>
-                <h3>Hello from navbar</h3>
-            </NavWrapper>
+                <Link className="App-header" to="/">
+                      Films
+                </Link>
+                <Link className="App-header" to="/cart">
+                        <span > 
+                        <i className="fas fa-cart-plus" />
+                         mon cadis
+                        </span>
+                </Link>
+            </div>
             )
     }
 }
 
-const NavWrapper = styled.nav`
-    background:var(--mainBlue);
-    .nav-link{
-       color:var(--mainWhite)!important;
-       font-size:1.3rem;
-    }
-`
